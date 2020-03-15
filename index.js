7eeb1c23b7a9eb4d820fed1c3dbae583679e7bf9
rwywawn <rwywawnw@gmail.com>
2020-03-15 19:31:20 -0400

    fixing git ignore

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const clanInfo = require("./apiHelper.js");
-const discordToken = require("./.gitignore/discordToken.js");
-const apiToken = require("./.gitignore/apiToken.js");
+const discordToken = require("./ignorefiles/discordToken.js");
+const apiToken = require("./ignorefiles/apiToken.js");
 const prefix = "!";
 
 client.once("ready", () => {
